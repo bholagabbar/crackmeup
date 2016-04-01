f17940ff28d6c1f474072468489769dc58bc7ff8
bholagabbar <shreyans.sheth@gmail.com>
2016-04-02 00:45:52 +0530

    Scraped jokesyou.com

--- crackmeup.py
+++ crackmeup.py
@@ -1,5 +1,6 @@
 import urllib
 import random
+import re
 from random import randint
 
 from bs4 import BeautifulSoup
@@ -46,11 +47,27 @@ def randomjokesDotcom():
 	joke = jokeSectionText[:jokeSectionText.index('Over')].strip()
 	return joke
 
-whichSite = randint(1,1)
-if whichSite:
-	print "\n" + jokesDotCCDotcom()
+def jokesYouDotcom():
+	urlToRead = "http://www.jokesyou.com/"
+	handle = urllib.urlopen(urlToRead)
+	htmlGunk =  handle.read()
+	soup = BeautifulSoup(htmlGunk, "html.parser")
+	joke = soup.findAll('div', {'class':'right'})[0].findAll('p')[0]
+	joke = str(joke)
+	#Regex replace
+	joke = re.sub(r'<p>', r'', joke)
+	joke = re.sub(r'</p>|<br/>', r'\n', joke)
+	return joke
+
+whichSite = randint(1,2)
+if whichSite == 1:
+	print jokesDotCCDotcom()
 	print "\nSource - jokes.cc.com"
 
+elif whichSite == 2:
+	print jokesYouDotcom()
+	print "\nSource - jokesyou.com"
+
 else:
-	print "\n" + randomjokesDotcom()
+	print randomjokesDotcom()
 	print "\nSource - randomjoke.com"
